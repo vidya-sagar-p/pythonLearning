ee143a1133ff41de68b5c88a85d982f3ef78dc13
da-vidya-sagar <120082681+da-vidya-sagar@users.noreply.github.com>
2024-01-22 23:58:20 +0530

    day 6

--- Reeborg_world/jump_hurdles_to_goal.py
+++ Reeborg_world/jump_hurdles_to_goal.py
@@ -1,3 +1,5 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
 def turn_right():
     turn_left()
     turn_left()
---
+++ Reeborg_world/variable_height_length_hurdles_to_goal.py
@@ -0,0 +1,34 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def climb():
+    turn_left()
+    while wall_on_right() is True:
+        move()
+
+
+def fall():
+    turn_right()
+    move()
+    turn_right()
+    move()
+    while wall_on_right() is True and at_goal() != True:
+        if front_is_clear() is True:
+            move()
+        else:
+            turn_left()
+
+
+# for i in range(6):
+while at_goal() != True:
+    if wall_in_front() is True and wall_on_right() is True:
+        climb()
+    elif right_is_clear() is True:
+        fall()
+    else:
+        move()
